bc36381f4a7117ea34336fc6d92e9e683c691638
Sadie L. Bartholomew <sadie.bartholomew@ncas.ac.uk>
2025-02-24 14:15:41 +0000

    Add timing and logging functionality

--- anvil.py
+++ anvil.py
@@ -1,9 +1,48 @@
+import functools
+import logging
+import os
 import sys
+
+from pprint import pformat
+from time import time
 
 import cf
 import nvector as nv
 
 
+# ----------------------------------------------------------------------------
+# Set up timing and logging
+# ----------------------------------------------------------------------------
+
+logger = logging.getLogger(__name__)
+
+
+def timeit(func):
+    """A decorator to measure and report function execution time."""
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        starttime = time()
+        output = func(*args, **kwargs)
+        endtime = time()
+        totaltime = endtime - starttime
+
+        # Note: using a print not log call here, so they always emerge. At
+        # release time we can subsume this into the logging system.
+        print(
+            f"\n_____ Time taken (in s) for {func.__name__!r} to run: "
+            f"{round(totaltime, 4)} _____\n"
+        )
+        return output
+
+    return wrapper
+
+
+# ----------------------------------------------------------------------------
+# Main procedure
+# ----------------------------------------------------------------------------
+
+@timeit
 def main():
     print("Running ANVIL")
 
