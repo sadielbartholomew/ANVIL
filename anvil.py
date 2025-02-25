6926603d2d5497cf428044f510cee92a7c676727
Sadie L. Bartholomew <sadie.bartholomew@ncas.ac.uk>
2025-02-25 00:29:48 +0000

    Add functions to calculate n-vector(s), singular & all across axis

--- anvil.py
+++ anvil.py
@@ -42,16 +42,6 @@ def timeit(func):
 # Core functions
 # ----------------------------------------------------------------------------
 
-def get_nvectors(construct):
-    """TODO
-    """
-    pass  # TODO
-
-
-# ----------------------------------------------------------------------------
-# Main procedure
-# ----------------------------------------------------------------------------
-
 def get_env_and_diagnostics_report():
     """Provide an optional report of environment and diagnostics.
 
@@ -69,6 +59,41 @@ def get_env_and_diagnostics_report():
     )
 
 
+def get_nvector(lats, lons):
+    """TODO."""
+    return nv.lat_lon2n_E(lats.data, lons.data)
+
+
+def get_nvectors_across_coord(field, across_latitude=True):
+    """TODO
+
+    If across_latitude is True the nvectors will be returned for every
+    latitude value of the field in order, else it will be returned for every
+    longitude value in order.
+    """
+    lats = field.coordinate("latitude")
+    lons = field.coordinate("longitude")
+
+    # Always take the first if the other coord (lat/lon) as the one
+    # to find n-vector for
+    nvectors = []
+    if across_latitude:
+        for datum in lats:
+            nvector = get_nvector(datum, lons[0])
+            nvectors.append(nvector)
+    else:
+        for datum in lons:
+            nvector = get_nvector(lats[0], lons)
+            nvectors.append(nvector)
+
+    print("n-vector list determined is:", nvectors)
+    return nvectors
+
+
+# ----------------------------------------------------------------------------
+# Main procedure
+# ----------------------------------------------------------------------------
+
 @timeit
 def main():
     """TODOO."""
@@ -77,7 +102,6 @@ def main():
     new ="\n"
 
     # 1. Initiate tool
-
     print(div, "Running ANVIL", div)
     print("Using environment of:")
     get_env_and_diagnostics_report()
@@ -85,26 +109,24 @@ def main():
     # 2. Get data to use.
     # Just use an example field for now, we only care about the regular lat-lon
     # grid and its resolution, not the data itself.
-    fl = cf.example_fields()
-    f = fl[0]
-    print(new, "Data is:")
+    f = cf.example_field(0)
+    print("Data is:")
     f.dump()
 
     # 3. Get all latitudes in the upper hemisphere - we can reflect about the
     # hemisphere to get all the lower hemisphere equivalents and then use
     # rotational symmetry of the Earth to get the information for each
     # longitude for a given latitude.
-    print(new, "Processing latitudes")
+    print("Processing latitudes")
     lats_key, lats = f.coordinate("latitude", item=True)
-    print("IS", lats_key, lats)
 
     # Subspacing to the upper hemisphere only
     kwargs = {lats_key: cf.ge(0)}  # greater than or equal to 0, i.e. equator
-    upper_hemi_lats = f.subspace(**kwargs)
-    print(new, "Upper hemisphere latitudes are:", upper_hemi_lats)
+    upper_hemi_lats_field = f.subspace(**kwargs)
+    print("Upper hemisphere latitudes are:", upper_hemi_lats_field)
 
     # 4. Get n-vectors for lat (at any, take first lon value) grid points
-    nvectors = get_nvectors(construct)
+    nvectors = get_nvectors_across_coord(upper_hemi_lats_field)
 
 
 
